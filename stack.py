f89fa90b12e6717e08d999043bf29c907fd8d5c8
Edward Lee <edwardneo@gmail.com>
2021-11-08 23:31:00 -0800

    Added stack data structure

---
+++ stack.py
@@ -0,0 +1,26 @@
+class Stack():
+    """A stack (FIFO)"""
+
+    def __init__(self, lst=''):
+        self.lst = lst
+    
+    def push(self, item):
+        self.lst.append(item)
+    
+    def pop(self):
+        if self.lst:
+            return self.lst.pop()
+        return None
+    
+    def __str__(self):
+        string = ''
+        for item in self.lst[:-1]:
+            string += item + ' '
+            
+        if self.lst:
+            string += f'[{self.lst[-1]}]'
+
+        return string
+    
+    def __repr__(self):
+        return f'Stack({self.lst})'
